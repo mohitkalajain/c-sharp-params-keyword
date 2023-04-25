63e0338aff8cf9092b971561c0e88ef5592bac7a
Mohit Kala <73868188+mohitkalajain@users.noreply.github.com>
2023-04-25 12:41:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Use_of___params__keyword
+{
+    //What is the purpose of “params” keyword
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Addition(10, 20, 30, 40);
+            Console.WriteLine(result);
+        }
+
+        //This code adds up all the numbers in an array and returns the total.
+        public static int Addition(params int[] numbers)
+        {
+            //Declare a variable to store the total
+            int total = 0;
+            //Loop through the array
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                //Add the current number to the total
+                total += numbers[i];
+            }
+            //Return the total
+            return total;
+        }
+    }
+}
